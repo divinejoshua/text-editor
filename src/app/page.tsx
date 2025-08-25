24e5396953a22b969e618f01087a0ca0e92dd64f
divinejoshua <divineuche399@gmail.com>
2025-08-25 11:49:59 +0100

    Added sfdt word branch

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,13 +7,40 @@ function Home() {
   const editorObj = useRef<DocumentEditorContainerComponent | null>(null);
   let contentChanged = false;
 
+  let sfdtContent = `
+  {"optimizeSfdt":true,"sec":[{"secpr":{"pw":612,"ph":792,"lm":72,"rm":72,"tm":72,"bm":72,"hd":36,"fd":36,"dfp":0,"doep":0,"bi":0,"bc":"NewPage","enf":2,"fnf":0,"rif":0,"rie":0,"ifn":1,"ien":1,"pgns":"Arabic","ncols":1,"eqw":1,"lbtc":0,"cols":[]},"b":[{"pf":{"bdrs":{"tp":{},"lt":{},"rg":{},"bt":{},"h":{},"v":{}},"stn":"Normal","lif":{}},"cf":{},"i":[{"cf":{"bi":false},"tlp":"hello world"}]}],"hf":{}}],"cf":{"b":false,"i":false,"fsz":11,"ff":"Arial","u":0,"st":0,"ba":0,"hc":0,"fszbi":11,"ffbi":"Calibri","ac":false,"ffa":"Arial","ffnfe":"Arial"},"pf":{"bdrs":{"tp":{},"lt":{},"rg":{},"bt":{},"h":{},"v":{}},"lin":0,"rin":0,"fin":0,"ta":0,"bs":0,"as":0,"ls":1,"lst":0,"ol":0,"bi":false,"klt":false,"kwn":false,"wc":true,"lif":{}},"fst":{},"tfl":{},"dtw":36,"tc":0,"enf":0,"hv":"","sv":"","fmt":0,"pt":0,"dhtml":0,"ffs":1,"comp":0,"stytbl":0,"bg":{"c":"#FFFFFF"},"sty":[{"n":"Normal","t":0,"pf":{"bdrs":{"tp":{},"lt":{},"rg":{},"bt":{},"h":{},"v":{}},"lif":{}},"cf":{},"nx":"Normal"},{"n":"Heading 1","t":0,"pf":{"bdrs":{"tp":{},"lt":{},"rg":{},"bt":{},"h":{},"v":{}},"lin":0,"rin":0,"fin":0,"ta":0,"bs":12,"as":0,"ls":1.0791666507720947,"lst":0,"ol":1,"lif":{}},"cf":{"fsz":16,"ff":"Calibri Light","fc":"#2F5496","ffa":"Calibri Light","ffnfe":"Calibri Light"},"b":"Normal","l":"Heading 1 Char","nx":"Normal"},{"n":"Heading 1 Char","t":1,"cf":{"fsz":16,"ff":"Calibri Light","fc":"#2F5496","ffa":"Calibri Light","ffnfe":"Calibri Light"},"b":"Default Paragraph Font"},{"n":"Default Paragraph Font","t":1,"cf":{}},{"n":"Heading 2","t":0,"pf":{"bdrs":{"tp":{},"lt":{},"rg":{},"bt":{},"h":{},"v":{}},"lin":0,"rin":0,"fin":0,"ta":0,"bs":2,"as":0,"ls":1.0791666507720947,"lst":0,"ol":2,"lif":{}},"cf":{"fsz":13,"ff":"Calibri Light","fc":"#2F5496","ffa":"Calibri Light","ffnfe":"Calibri Light"},"b":"Normal","l":"Heading 2 Char","nx":"Normal"},{"n":"Heading 2 Char","t":1,"cf":{"fsz":13,"ff":"Calibri Light","fc":"#2F5496","ffa":"Calibri Light","ffnfe":"Calibri Light"},"b":"Default Paragraph Font"},{"n":"Heading 3","t":0,"pf":{"bdrs":{"tp":{},"lt":{},"rg":{},"bt":{},"h":{},"v":{}},"lin":0,"rin":0,"fin":0,"ta":0,"bs":2,"as":0,"ls":1.0791666507720947,"lst":0,"ol":3,"lif":{}},"cf":{"fsz":12,"ff":"Calibri Light","fc":"#1F3763","ffa":"Calibri Light","ffnfe":"Calibri Light"},"b":"Normal","l":"Heading 3 Char","nx":"Normal"},{"n":"Heading 3 Char","t":1,"cf":{"fsz":12,"ff":"Calibri Light","fc":"#1F3763","ffa":"Calibri Light","ffnfe":"Calibri Light"},"b":"Default Paragraph Font"},{"n":"Heading 4","t":0,"pf":{"bdrs":{"tp":{},"lt":{},"rg":{},"bt":{},"h":{},"v":{}},"lin":0,"rin":0,"fin":0,"ta":0,"bs":2,"as":0,"ls":1.0791666507720947,"lst":0,"ol":4,"lif":{}},"cf":{"i":true,"ff":"Calibri Light","fc":"#2F5496","ffa":"Calibri Light","ffnfe":"Calibri Light"},"b":"Normal","l":"Heading 4 Char","nx":"Normal"},{"n":"Heading 4 Char","t":1,"cf":{"i":true,"ff":"Calibri Light","fc":"#2F5496","ffa":"Calibri Light","ffnfe":"Calibri Light"},"b":"Default Paragraph Font"},{"n":"Heading 5","t":0,"pf":{"bdrs":{"tp":{},"lt":{},"rg":{},"bt":{},"h":{},"v":{}},"lin":0,"rin":0,"fin":0,"ta":0,"bs":2,"as":0,"ls":1.0791666507720947,"lst":0,"ol":5,"lif":{}},"cf":{"ff":"Calibri Light","fc":"#2F5496","ffa":"Calibri Light","ffnfe":"Calibri Light"},"b":"Normal","l":"Heading 5 Char","nx":"Normal"},{"n":"Heading 5 Char","t":1,"cf":{"ff":"Calibri Light","fc":"#2F5496","ffa":"Calibri Light","ffnfe":"Calibri Light"},"b":"Default Paragraph Font"},{"n":"Heading 6","t":0,"pf":{"bdrs":{"tp":{},"lt":{},"rg":{},"bt":{},"h":{},"v":{}},"lin":0,"rin":0,"fin":0,"ta":0,"bs":2,"as":0,"ls":1.0791666507720947,"lst":0,"ol":6,"lif":{}},"cf":{"ff":"Calibri Light","fc":"#1F3763","ffa":"Calibri Light","ffnfe":"Calibri Light"},"b":"Normal","l":"Heading 6 Char","nx":"Normal"},{"n":"Heading 6 Char","t":1,"cf":{"ff":"Calibri Light","fc":"#1F3763","ffa":"Calibri Light","ffnfe":"Calibri Light"},"b":"Default Paragraph Font"},{"n":"Heading 7","t":0,"pf":{"bdrs":{"tp":{},"lt":{},"rg":{},"bt":{},"h":{},"v":{}},"lin":0,"rin":0,"fin":0,"ta":0,"bs":2,"as":0,"ls":1.0791666507720947,"lst":0,"ol":7,"lif":{}},"cf":{"i":true,"ff":"Calibri Light","fc":"#1F3763","ffa":"Calibri Light","ffnfe":"Calibri Light"},"b":"Normal","l":"Heading 7 Char","nx":"Normal"},{"n":"Heading 7 Char","t":1,"cf":{"i":true,"ff":"Calibri Light","fc":"#1F3763","ffa":"Calibri Light","ffnfe":"Calibri Light"},"b":"Default Paragraph Font"},{"n":"Heading 8","t":0,"pf":{"bdrs":{"tp":{},"lt":{},"rg":{},"bt":{},"h":{},"v":{}},"lin":0,"rin":0,"fin":0,"ta":0,"bs":2,"as":0,"ls":1.0791666507720947,"lst":0,"ol":8,"lif":{}},"cf":{"fsz":10.5,"ff":"Calibri Light","fc":"#333333","ffa":"Calibri Light","ffnfe":"Calibri Light"},"b":"Normal","l":"Heading 8 Char","nx":"Normal"},{"n":"Heading 8 Char","t":1,"cf":{"fsz":10.5,"ff":"Calibri Light","fc":"#333333","ffa":"Calibri Light","ffnfe":"Calibri Light"},"b":"Default Paragraph Font"},{"n":"Heading 9","t":0,"pf":{"bdrs":{"tp":{},"lt":{},"rg":{},"bt":{},"h":{},"v":{}},"lin":0,"rin":0,"fin":0,"ta":0,"bs":2,"as":0,"ls":1.0791666507720947,"lst":0,"ol":9,"lif":{}},"cf":{"i":true,"fsz":10.5,"ff":"Calibri Light","fc":"#333333","ffa":"Calibri Light","ffnfe":"Calibri Light"},"b":"Normal","l":"Heading 9 Char","nx":"Normal"},{"n":"Heading 9 Char","t":1,"cf":{"i":true,"fsz":10.5,"ff":"Calibri Light","fc":"#333333","ffa":"Calibri Light","ffnfe":"Calibri Light"},"b":"Default Paragraph Font"},{"n":"Header","t":0,"pf":{"bdrs":{"tp":{},"lt":{},"rg":{},"bt":{},"h":{},"v":{}},"as":0,"ls":1,"lst":0,"lif":{}},"cf":{},"b":"Normal","nx":"Header"},{"n":"Footer","t":0,"pf":{"bdrs":{"tp":{},"lt":{},"rg":{},"bt":{},"h":{},"v":{}},"as":0,"ls":1,"lst":0,"lif":{}},"cf":{},"b":"Normal","nx":"Footer"}],"li":[],"al":[],"cm":[],"r":[],"cx":[],"imgs":{}}
+  `
+
   function onContentChange() {
     contentChanged = true;
     console.log("contentChanged", contentChanged);
+
   }
 
   const onSave = () => {
     editorObj.current?.documentEditor.save("Sample", "Docx");
+
+    // Get SFDT string
+    const sfdt = editorObj.current?.documentEditor.serialize();
+    console.log(sfdt);
+
+    // Save file to server
+    // Export the document as a Word Blob
+    editorObj.current?.documentEditor.saveAsBlob('Docx').then((blob) => {
+      console.log(blob); // This is your Word document as a Blob
+
+      // Send to your server
+      const formData = new FormData();
+      formData.append('file', blob, 'sample.docx');
+
+      fetch('/api/upload', {
+          method: 'POST',
+          body: formData,
+      })
+      .then(res => res.json())
+      .then(data => console.log('Uploaded to server:', data))
+      .catch(err => console.error('Upload error:', err));
+    });
   };
 
   function onCreate() {
@@ -21,6 +48,14 @@ function Home() {
       fontFamily: 'Arial',
     };
     editorObj.current?.documentEditor.setDefaultCharacterFormat(defaultCharacterFormat);
+
+
+    // load your default document here
+    const data = sfdtContent;
+    // Open the default document
+    if (sfdtContent) {
+      editorObj.current?.documentEditor.open(data);
+    }
   }
 
   return (
